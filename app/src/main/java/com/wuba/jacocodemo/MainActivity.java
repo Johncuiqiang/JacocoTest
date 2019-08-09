e132aefb770d21d49800419e8b76056a3f3b5aba
cuiqiang01 <cuiqiang01@58ganji.com>
2019-08-09 16:33:21 +0800

    init

---
+++ app/src/main/java/com/wuba/jacocodemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.wuba.jacocodemo;
+
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import java.util.List;
+
+import jaygoo.jacocohelper.JacocoHelper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private View mBtnTest;
+    private static final String PROJECT_PATH = "/Users/cuiqiang/developer/demo/JacocoDemo/app" +
+            "/build/outputs/code-coverage/";
+    private View mBtnGenerate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        JacocoHelper.init(PROJECT_PATH,true);
+        mBtnTest = findViewById(R.id.btn_test);
+        mBtnGenerate = findViewById(R.id.btn_generate);
+        Test test = new Test();
+        test.onTest();
+    }
+
+    private void initData() {
+        mBtnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("1111","测试按钮");
+            }
+        });
+        mBtnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  JacocoHelper.generateEcFile(true);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/wuba/jacocodemo/Test.java
@@ -0,0 +1,13 @@
+package com.wuba.jacocodemo;
+
+/**
+ * @Author : cuiqiang
+ * @DATE : 2019/8/9 10:37
+ * @Description :
+ */
+public class Test {
+
+    public void onTest(){
+
+    }
+}
